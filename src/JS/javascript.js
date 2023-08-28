da444eb747eab96bf7147b267e4df2fecb512005
Memedroid Eventos <123564826+Cristian-Nascimento-Mauricio@users.noreply.github.com>
2023-08-28 17:24:54 -0300

    upgrade javascript code

--- src/JS/javascript.js
+++ src/JS/javascript.js
@@ -1,23 +1,46 @@
-import { scheduleDay } from "./objects.js"
+import { item, scheduleDay } from "./objects.js"
 
 const time = document.getElementById("time")
 const schedule = document.querySelector('.schedule')
-const add = document.querySelector('.add')
-const date = new Date
+const button = document.querySelector(".div-button")
+let arrayList = []
 
-add.addEventListener('click', ()=>{
+setDate()
+
+button.addEventListener('click',()=>{
+    document.querySelector('.schedule-of-day-list').innerHTML += '<div></div>'
     console.log("Cliked")
 })
 
-time.innerText = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}` 
-
 for(var i = 0; i < 3;i++){
-    schedule.innerHTML += new scheduleDay("Título testando",i).html
-    
+    arrayList.push(new scheduleDay("Título testando",i,[ new item("texto"),new item("Outro texto"), new item("Mais texto") ]))
+}
+
+arrayList.forEach(element => {
+    schedule.innerHTML += element.html
+})
+
+document.querySelectorAll(".item input").forEach(element => {
+    element.addEventListener('click',  ()=>{
+
+    var valor = 0
+
+    element.parentElement.parentElement.querySelectorAll(".item input").forEach(subelement =>{
+        valor += subelement.checked ? 1:0
+
+        element.parentElement.parentElement.parentElement.querySelector(".container .progress p").innerHTML = (Math.round( (valor / element.parentElement.parentElement.querySelectorAll(".item").length)*100) + "%")
+    })
+    })
+})
+
+function setDate(){
+    const date = new Date
+
+    time.innerText = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}` 
+
 }
 
 const schedule_day = document.querySelectorAll(".schedule-day")
-
 
 schedule_day.forEach(element => {
     element.querySelector(".container").addEventListener('click', () => {
--- src/JS/objects.js
+++ src/JS/objects.js
@@ -1,38 +1,46 @@
 export class scheduleDay {
 
-    constructor(title,id){
-        this.title = title
-        this.progress = 100
-        this.id = id
-        this.list = []
-        this.html = `
-        <div class="schedule-day" id="${this.id}">
-          <div class="container">
-              <div class="title">
-                  <p>${this.title}</p>
-              </div>
-              <div class="progress">
-                <p>${this.progress}%</p>
-              </div>
-          </div>
-          <div class="list">
-            <div class="item">
-            <input type="checkbox">
-            <p>Texto generico</p>
-            </div><div class="item">
-            <input type="checkbox">
-            <p>Texto generico</p>
-            </div><div class="item">
-            <input type="checkbox">
-            <p>Texto generico</p>
-          </div>
-          </div>
+    constructor(title,id,list){
+      this.title = title
+      this.progress = 100
+      this.id = id
+      this.list = list
+      this.itens = ''
+      list.forEach(element => {
+        this.itens += element.html
+      });
+       
+      this.html = `
+      <div class="schedule-day" id="${this.id}">
+        <div class="container">
+            <div class="title">
+                <p>${this.title}</p>
+            </div>
+            <div class="progress">
+              <p>${this.progress}%</p>
+            </div>
         </div>
-
+        <div class="list">
+          ${this.itens}
+        </div>
       </div>
-        `
 
-    }
+    </div>
+    ` 
+  }
 
 }
 
+export class item {
+
+  constructor(title){
+    this.title = title
+    this.html = `
+    <div class="item">
+      <input type="checkbox" >
+      <p>${this.title}</p>
+    </div>
+    `
+  }
+
+}
